2ef4dbfc58966dae57f823ea85b8e617905dfc34
Simeon Georev <simeon.georev.97@hotmail.com>
2026-03-21 20:08:35 +0200

    JS Exercise N5 - done

--- js/05-dom-introduction-exercise/07. Hell's Kitchen/hells-kitchen.js
+++ js/05-dom-introduction-exercise/07. Hell's Kitchen/hells-kitchen.js
@@ -1,21 +1,61 @@
+/**
+ * Very hard task with nested objects + sorting and filtering objects
+ */
 function solve() {
-    let text = document.querySelector("#inputs textarea").value
-    text = text.substring(2, text.length - 2)
-    console.log(text)
+    let text = document.querySelector("#inputs textarea").value;
+    text = text.substring(2, text.length - 2);
 
-    let restaurantsRaw = text.split("\",\"")
-    console.log(restaurantsRaw);
+    let restaurantsRaw = text.split("\",\"");
     
+    let bestRestaurantsEl = document.querySelector("#bestRestaurant p");
+    let workersEl = document.querySelector("#workers p");
 
-    let bestRestaurantsEl = document.querySelector("#bestRestaurant p")
-    let workersEl = document.querySelector("#workers p")
+    let restaurants = {}
 
     restaurantsRaw.forEach(restaurantRaw => {
-        restaurantRaw = restaurantRaw.split(" - ")
-        restaurantName = restaurantRaw[0]
-        restaurantWorkers = restaurantRaw[1]
-        console.log(restaurantName);
-        console.log(restaurantWorkers);
-        
+        // "PizzaHut - Peter 500, George 300, Mark 800"
+        restaurantRaw = restaurantRaw.split(" - ");
+
+        let restaurantName = restaurantRaw[0];
+        let restaurantWorkers = restaurantRaw[1].split(", ").map(x => x.split(" "));
+        const newWorkers = Object.fromEntries(restaurantWorkers);
+
+        if (restaurants[restaurantName]) {
+            Object.assign(restaurants[restaurantName], newWorkers);
+        }
+        else {
+            restaurants[restaurantName] = newWorkers;
+        }
     });
+
+    const sortedRestaurants = Object.entries(restaurants).sort((a, b) => getAverageSalary(b[1]) - getAverageSalary(a[1]));
+
+    const bestRestaurant = sortedRestaurants[0][0]
+    const bestEmployeeObj = sortedRestaurants[0][1]
+
+    const bestAverageSalary = getAverageSalary(bestEmployeeObj)
+
+    const employeesSorted = Object.entries(bestEmployeeObj).sort((a, b) => Number(b[1]) - Number(a[1]))
+
+    const bestSalary = Number(employeesSorted[0][1])
+
+    bestRestaurantsEl.innerText = `Name: ${bestRestaurant} Average Salary: ${bestAverageSalary.toFixed(2)} Best Salary: ${bestSalary.toFixed(2)}`
+    
+    // `Name: {worker name} With Salary: {worker salary} Name: {worker2 name} With Salary: {worker2 salary} Name: {worker3 name} With Salary: {worker3 salary}…`
+    let result = []
+    console.log(employeesSorted);
+    
+    employeesSorted.forEach(emp => {
+        result.push(`Name: ${emp[0]} With Salary: ${emp[1]}`)
+    }) 
+
+    workersEl.innerText = result.join(" ")
+
+        // get average salary
+    function getAverageSalary(employeeObj) {
+        const salaries = Object.values(employeeObj)
+        const sum = salaries.reduce((currentSum, a) => currentSum + Number(a), 0)
+        const average = sum / salaries.length
+        return average
+    }
 }
--- js/05-dom-introduction-exercise/08. Generate Report/generate-report.js
+++ js/05-dom-introduction-exercise/08. Generate Report/generate-report.js
@@ -1,3 +1,39 @@
 function solve() {
-    //TODO
+    const headersEls = document.querySelectorAll("table tr input")
+
+    let headersArr = []
+
+    for (const el of headersEls) {
+        headersArr.push({
+            name: el.name,
+            checked: el.checked
+        })
+    }
+
+    const rowsEls = document.querySelectorAll("table tbody tr")
+
+    let results = []
+
+    for (let rowI = 0; rowI < rowsEls.length; rowI++) {
+        const cells = rowsEls[rowI].children
+
+        const empInfoObj = {}
+
+        for (let colI = 0; colI < cells.length; colI++) {
+            const headerStatus = headersArr[colI].checked
+            if (!headerStatus) {
+                continue
+            }
+
+            const headerName = headersArr[colI].name
+            const cellText = cells[colI].innerText
+
+            empInfoObj[headerName] = cellText
+        }
+        
+        results.push(empInfoObj)
+        
+    }
+
+    document.getElementById("output").textContent = JSON.stringify(results)
 }
